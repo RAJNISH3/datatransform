02071f929b3e0952b83e6717d89951336e901a37
RAJNISH TIWARI <rajnish.tiwari01@sap.com>
2023-09-30 19:26:50 +0530

    casandra code

--- src/main/java/com/lbn/companion/dataprocess/WordCountingApp.java
+++ src/main/java/com/lbn/companion/dataprocess/WordCountingApp.java
@@ -12,6 +12,7 @@ import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.streaming.OutputMode;
 import org.apache.spark.sql.streaming.StreamingQuery;
 import org.apache.spark.sql.streaming.StreamingQueryException;
 import org.apache.spark.streaming.api.java.JavaDStream;
@@ -46,28 +47,29 @@ public class WordCountingApp {
            // CassandraConnector connector = CassandraConnector.apply(spark.sparkContext().getConf());
 
             SparkConf sparkConf = new SparkConf();
-              sparkConf.setMaster("local[2]");
+            sparkConf.setMaster("local[2]");
             sparkConf.setAppName("WordCountingApp");
             sparkConf.set("spark.cassandra.connection.host", "127.0.0.1");
             sparkConf.set("spark.cassandra.connection.port", "9042");
 
-//            wordCounts.writeStream()
-//                    .option("checkpointLocation", checkpointDir.toString)
-//                    .format("org.apache.spark.sql.cassandra")
-//                    .option("keyspace", messages)
-//                    .option("table", "kv")
-//                    .outputMode(OutputMode.Update)
-//                    .start();
+           wordCounts.writeStream()
+                 //  .option("checkpointLocation", checkpointDir.toString)
+                   .format("org.apache.spark.sql.cassandra")
+                   .option("keyspace", "messages")
+                   .option("table", "test")
+                   .outputMode(OutputMode.Update())
+                   .start();
 
 
-//            wordCounts.write()
-//                    .format("org.apache.spark.sql.cassandra")
-//                    .options(new HashMap<>() {{
-//                        put("keyspace", "count");
-//                        put("table", "wordcount");
-//                    }})
-//                    .mode(SaveMode.Append)
-//                    .save();
+        //    wordCounts.write()
+        //            .format("org.apache.spark.sql.cassandra")
+        //            .options(new HashMap<>() {{
+        //                put("keyspace", "messages");
+        //                put("table", "wordcount");
+        //            }})
+        //            .mode(SaveMode.Append)
+        //            .save();
+
 
             // Start running the query that prints the running counts to the console
             StreamingQuery query = wordCounts.writeStream()
@@ -76,7 +78,6 @@ public class WordCountingApp {
                     .start();
 
             query.awaitTermination();
-
-                System.out.println("End of application");
+            System.out.println("End of application");
         }
 }
